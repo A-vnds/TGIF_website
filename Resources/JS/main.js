3c86053cbd1aa47286cad015890cf6b887807b51
A-vnds <avenidis@me.com>
2018-11-02 12:09:55 +0100

    all filters done

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,50 +1,36 @@
 const members = data.results[0].members
-
-
 const partySelector = document.getElementById("selectParty");
-
 const stateSelector = document.getElementById('selectState');
 
+let tableResults = document.getElementById('senate_table');
 
 const checkDem = document.getElementById('checkDem');
 const checkRep = document.getElementById('checkRep');
 const checkInd = document.getElementById('checkInd');
 
-console.log(partySelector);
-console.log(stateSelector);
-console.log(checkDem);
-
-
-
-
-
 createTable(members);
-partySelector.addEventListener("change", filterArray);
-stateSelector.addEventListener("change", filterArray);
+
+partySelector.addEventListener("change", zeroMatches);
+stateSelector.addEventListener("change", zeroMatches);
 
 
 
+// Create Main Table Function
 
-
-
-// Create Table Function
-
-console.log(data)
 function createTable(data) {
     let html = "<table border='1|1'>";
 
     for (let i = 0; i < data.length; i++) {
-        if (data[i].middle_name !=  null)
-        {
-        html += "<tr>";
-        html += "<td>" + (data[i].first_name + ' ' + data[i].middle_name + ' ' + data[i].last_name).link(data[i].url) + "</td>";
-        html += "<td>" + data[i].party + "</td>";
-        html += "<td>" + data[i].state + "</td>";
-        html += "<td>" + data[i].seniority + "</td>";
-        html += "<td>" + data[i].votes_with_party_pct + "</td>";
-        html += "</tr>"; 
-        } else 
-        html += "<tr>";
+        if (data[i].middle_name != null) {
+            html += "<tr>";
+            html += "<td>" + (data[i].first_name + ' ' + data[i].middle_name + ' ' + data[i].last_name).link(data[i].url) + "</td>";
+            html += "<td>" + data[i].party + "</td>";
+            html += "<td>" + data[i].state + "</td>";
+            html += "<td>" + data[i].seniority + "</td>";
+            html += "<td>" + data[i].votes_with_party_pct + "</td>";
+            html += "</tr>";
+        } else
+            html += "<tr>";
         html += "<td>" + (data[i].first_name + ' ' + data[i].last_name).link(data[i].url) + "</td>";
         html += "<td>" + data[i].party + "</td>";
         html += "<td>" + data[i].state + "</td>";
@@ -52,87 +38,66 @@ function createTable(data) {
         html += "<td>" + data[i].votes_with_party_pct + '%' + "</td>";
         html += "</tr>";
     }
-    
+
     html += "</tbody>";
     document.getElementById("senate_table").innerHTML = html;
-    
+
 
 }
 
-createTable(members);
-
-
-
-// Filter Array based on Gender
-
-function filterArray2() {
-
-    let filteredArray = [];
-    
-    for (let i = 0; i < members.length; i++) {
-        
-        if (members[i].party == partySelector.value || partySelector.value == 'All') 
-        
-        
-        {
-            if (members[i].state == stateSelector.value || stateSelector.value == 'All'){
-                filteredArray.push(members[i]); }
-            }
-        }
-    createTable(filteredArray);
-}
-
+// Filter Array based on State and Party 
 
 function filterArray() {
 
     let filteredArray = [];
-    
+
     for (let i = 0; i < members.length; i++) {
-        
+
         if (members[i].state == stateSelector.value || stateSelector.value == 'All') {
-            
-            if ((members[i].party == 'D') && (checkDem.checked == true))  {
-                filteredArray.push(members[i]);} 
-            else if ((members[i].party == 'R') && (checkRep.checked == true)) {
-                filteredArray.push(members[i]);}
-            else if (members[i].party == 'I') && (checkInd.checked == true) {
-                filteredArray.push(members[i]);}
+
+            if ((members[i].party == 'D') && (checkDem.checked == true)) {
+                filteredArray.push(members[i]);
+            } else if ((members[i].party == 'R') && (checkRep.checked == true)) {
+                filteredArray.push(members[i]);
+            } else if ((members[i].party == 'I') && (checkInd.checked == true)) {
+                filteredArray.push(members[i]);
             }
-        }
-    createTable(filteredArray);
-}
 
- 
-        
-/*
-
-function filterArray() {
-
-    let filteredArray = [];
-    
-    for (let i = 0; i < data.length; i++) {
-        if (members[i].gender == genderSelector.value || genderSelector.value == 'All') {
-            if (ageSelector.value == "older" && data[i].age >= 30 ) {
-                filteredArray.push(data[i]);
-            } else if (ageSelector.value == "younger" && data[i].age < 30) {
-                filteredArray.push(data[i]);
-            } else if (ageSelector.value == 'All') {
-                filteredArray.push(data[i]);
-            }
         }
     }
     createTable(filteredArray);
 }
 
+// Reset Function based on different cases
 
+function caseReset() {
+    if ((stateSelector.value == 'All' && (checkDem.checked == false) && (checkRep.checked == false) && (checkInd.checked == false))) {
+        resetTableArr();
+    } else {
+        filterArray();
+    }
+    }
 
+// General Reset Function of the Table
 
+function resetTableArr() {
 
+    stateSelector.value = 'All'
+    checkDem.checked = true;
+    checkRep.checked = true;
+    checkInd.checked = true;
+    filterArray();
+}
 
+// Message Function for Zero Matches, the main function called on the Event Listener at the Top
 
-*/
-
-
-
-
-
+function zeroMatches() {
+    
+    filterArray();
+    
+    if (!tableResults.hasChildNodes()) {
+        alert("There are no matches for this values, the table will be reset");
+        resetTableArr();
+    }}
+    
+ 
