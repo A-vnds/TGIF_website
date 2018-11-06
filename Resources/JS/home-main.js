fd3532f09897ad7e21e0ef674375a91788e8f3fa
A-vnds <avenidis@me.com>
2018-11-06 12:08:14 +0100

    Task about Public Data Display Done

--- Resources/JS/home-main.js
+++ Resources/JS/home-main.js
@@ -12,3 +12,29 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+
+
+let button1 = document.getElementById('myButton1');
+let button2 = document.getElementById('myButton2');
+let button3 = document.getElementById('myButton3');
+
+
+button1.addEventListener('click', changeText);
+button2.addEventListener('click', changeText);
+button3.addEventListener('click', changeText);
+
+
+function changeText() {
+    if (this.innerHTML === 'Read More'){
+        this.innerHTML = 'Read Less';
+    }  else {
+        this.innerHTML = 'Read More';
+    }
+}
+
+
+
+
+
+
