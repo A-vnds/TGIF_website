198d6e136fae70510061f9a56727730389f2b633
A-vnds <avenidis@me.com>
2018-11-19 15:32:31 +0100

    Final TGIF

--- Resources/JS/stats2.js
+++ Resources/JS/stats2.js
@@ -201,11 +201,7 @@ var app = new Vue({
             },
 
         
-        
-        
-        
-        
-        
+ 
          
         
     },
@@ -213,3 +209,21 @@ var app = new Vue({
 
 })
 
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myScrollBtn").style.display = "block";
+    } else {
+        document.getElementById("myScrollBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
